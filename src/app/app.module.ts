301407167cbc2a348ad80956c7f5c5edee7bd1bb
flandrea <flande14@gmail.com>
2021-01-21 18:20:19 -0500

    initial commit for lab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ListComponent } from './list/list.component';
+// import { ListItemsComponent } from './list-items/list-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    // ListItemsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
